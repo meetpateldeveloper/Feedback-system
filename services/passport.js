197d784b2641c57789e58a20b8f858081e9107eb
Meet Patel <m_patel175312@fanshaweonline.ca>
2023-05-09 14:44:02 -0400

    add authentication using google Auth

---
+++ services/passport.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model('users');
+
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback",
+      },
+      (accessToken, refreshToken, profile, done) => {
+        User.findOne({googleId: profile.id}).then((existingUser)=>{
+          if(existingUser){}
+          else{
+            new User({googleId: profile.id}).save();
+          }
+        })
+      }
+    )
+  );
+  
